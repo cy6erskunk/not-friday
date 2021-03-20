90360ee70b8fbc278c4a52ce8834048a588fff0b
Igor Shevchenko <cyberskunk@gmail.com>
2021-03-20 18:08:08 +0200

    Initial commit

---
+++ api/index.ts
@@ -0,0 +1,21 @@
+import {DateTime} from 'luxon';
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import {tzNames, getCountriesByTz} from './data';
+
+const now = DateTime.local()
+
+const nonFridayTZs = tzNames.filter((tzName) => now.setZone(tzName).weekdayLong !== 'Friday')
+
+const countriesSet = new Set();
+
+nonFridayTZs.forEach((tzName) => 
+  getCountriesByTz(tzName).
+  forEach((countryName) => countriesSet.add(countryName)))
+
+const countries = Array.from(countriesSet);
+const randomCountry = countries[Math.floor(Math.random() * countries.length)]
+
+export default (request: VercelRequest, response: VercelResponse) => {
+  response.status(200).send(randomCountry)
+}
+
