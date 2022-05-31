ba681ab564cc82b6473222b42865a0ea94e1a101
Igor <cyberskunk@gmail.com>
2022-05-31 11:12:27 +0300

    Tune colors to a more gloomy mode

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 import './style.css'
 import {getRandomCountry} from './api/index.mjs'
 
+const SELECTED_COLOR = 'salmon'
+
 const country = getRandomCountry();
 
 const title = document.createElement('h3');
 title.textContent = `At least not in ${country.name}`; 
 document.querySelector('#app').appendChild(title);
 if (document.querySelector(`#${country.code}`)) {
-  document.querySelector(`#${country.code}`).style.fill = 'red';
+  document.querySelector(`#${country.code}`).style.fill = SELECTED_COLOR;
 } else if (document.querySelectorAll(`path[name='${country.name}']`).length) {
   Array.from(document.querySelectorAll(`path[name='${country.name}']`))
-    .forEach(elt => elt.style.fill = 'red')
+    .forEach(elt => elt.style.fill = SELECTED_COLOR)
 } else {
   Array.from(document.querySelectorAll(`path[class='${country.name}']`))
-    .forEach(elt => elt.style.fill = 'red')
+    .forEach(elt => elt.style.fill = SELECTED_COLOR)
 }
